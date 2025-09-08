57870b292d129c62da6c438c3c8c37b02cb44044
Kuba <meowmme@gmail.com>
2025-09-08 11:49:56 +0200

    fix navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Counter from './pages/Counter.tsx'
 import Calculator from './pages/Calculator.tsx'
 import Home from './pages/Home.tsx'
+import Todo from './pages/Todo.tsx'
 
 function App() {
   return (
     <>
-      <div className='grid grid-cols-1'>
+      <div className='grid grid-cols-1 min-w-lg items-center justify-center'>
         <Router>
-          <nav className='font-bold text-xl bg-gray-800 px-5 py-2 flex gap-6 w-min mb-5 justify-center rounded-2xl'>
+          <nav className='font-bold text-xl bg-gray-800 px-5 py-2 flex gap-6 mb-5 justify-center rounded-2xl'>
             <Link to = "/" className="nav-item">Home</Link>
             <Link to = "/counter" className="nav-item">Counter</Link>
             <Link to = "/calculator" className="nav-item">Calculator</Link>
+            <Link to = "/todo" className='nav-item'>Todo</Link>
           </nav>
 
           <div className='flex min-h-screen justify-center'>
@@ -19,6 +21,7 @@ function App() {
               <Route path = "*" element = {<Home />} />
               <Route path = "/counter" element = {<Counter />} />
               <Route path = "/calculator" element = {<Calculator />} />
+              <Route path = "/todo" element = {<Todo />}></Route>
             </Routes>
           </div>
         </Router>
--- src/pages/Counter.tsx
+++ src/pages/Counter.tsx
@@ -28,7 +28,7 @@ export default function Counter(){
                 onClick={() => setCount((count) => count + 1)}>Add</button>
             <button className="bg-red-700 hover:bg-red-800 transition duration-500 ease-in-out text-gray-50 text-shadow-md text-shadow-gray-600 hover:text-gray-100 font-bold py-2 px-5 mx-2 rounded"
                 onClick={() => setCount((count) => count - 1)}>Subtract</button>
-            <button className="bg-blue-400 hover:bg-blue-500 transition duration-500 ease-in-out text-gray-50 text-shadow-md text-shadow-gray-600 hover:text-gray-100 font-bold py-2 px-5 mx-2 rounded"
+            <button className="bg-blue-700 hover:bg-blue-800 transition duration-500 ease-in-out text-gray-50 text-shadow-md text-shadow-gray-600 hover:text-gray-100 font-bold py-2 px-5 mx-2 rounded"
                 onClick={() => setCount(0)}>Reset</button>
             <p className="flex items-center justify-center text-shadow-md text-3xl py-2"
                 >{count}</p>
