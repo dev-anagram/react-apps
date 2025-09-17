73675f4965a756ffd0d621d9021f52753436a7ce
Kuba <meowmme@gmail.com>
2025-09-17 22:48:33 +0200

    weather style finish

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,50 +1,57 @@
-import { Droplets, Eye, EyeClosed, Wind } from "lucide-react";
+import { Droplets, Eye, Wind, Sun, CloudRain, Droplet, Zap } from "lucide-react";
 
 export default function Weather(){
     return(
         // wrapper
-        <div className="font-mono text-center items-center align-middle text-black grid grid-cols-1 p-6 bg-yellow-400 rounded-2xl w-auto h-min relative border-solid border-3 border-gray-500">
+        <div className="font-mono text-center items-center align-middle text-black grid grid-cols-1 p-6 bg-linear-to-br to-yellow-400 from-white rounded-4xl w-auto h-min relative border-solid border-3">
             {/* City */}
-            <div className="font-extrabold text-lg mb-3">Paris</div>
+            <div className="font-bold text-2xl mb-3">Paris</div>
             {/* date */}
-            <div className="font-medium text-sm bg-black text-yellow-400 rounded-xl px-3 py-0.5 my-2 mx-auto w-min text-nowrap">friday 20 january</div>
+            <div className="font-medium text-lg bg-black text-yellow-400 rounded-xl px-3 py-0.5 my-2 mx-auto w-min text-nowrap">friday 20 january</div>
             {/* weather */}
             <div className="font-bold text-lg">Sunny</div>
             {/* temperature */}
             <div className="text-9xl">31&#176;</div>
             {/* wind / humidity / visibility */}
-            <div className="grid grid-cols-3 gap-1 bg-black rounded-xl text-yellow-400 px-5 py-4">
-                <div className="grid grid-cols-1  justify-center">
-                    <Wind className="mx-auto" />
-                    3km/h
+            <div className="grid grid-cols-3 gap-1 bg-black rounded-2xl text-yellow-400 px-5 py-2">
+                <div className="weather-attribute">
+                    <Wind className="mx-auto " size={40}/>
+                    <p className="weather-item">3km/h</p>
+                    <p className="weather-item-desc">Wind</p>
                 </div>
-                <div>
-                    <Droplets className="mx-auto" />
-                    48%
+                <div className="weather-attribute">
+                    <Droplets className="mx-auto " size={40}/>
+                    <p className="weather-item">48%</p>
+                    <p className="weather-item-desc">Humidity</p>
                 </div>
-                <div>
-                    <Eye className="mx-auto" />
-                    1.6km
+                <div className="weather-attribute">
+                    <Eye className="mx-auto" size={40}/>
+                    <p className="weather-item">1.6km</p>
+                    <p className="weather-item-desc">Visibility</p>
                 </div>
             </div>
             {/* forecast */}
-            <div>Weekly forecast</div>
-            <div>
-                <div>
-                    26&#8451;
-                    21 jan
+            <div className="font-bold text-lg mt-2">Weekly forecast</div>
+            <div className="flex justify-center gap-2">
+                <div className="weather-forecast-item">
+                    <p className="weather-forecast-temp">26&#176;</p>
+                    <Sun className="mx-auto "/>
+                    <p className="weather-forecast-date">21 jan</p>
                 </div>
-                <div>
-                    25&#8451;
-                    22 jan
+                <div className="weather-forecast-item">
+                    <p className="weather-forecast-temp">25&#176;</p>
+                    <CloudRain className="mx-auto"/>
+                    <p className="weather-forecast-date">22 jan</p>
                 </div>
-                <div>
-                    27&#8451;
-                    23 jan
+                <div className="weather-forecast-item">
+                    <p className="weather-forecast-temp">27&#176;</p>
+                    <Droplet className="mx-auto" />
+                    <p className="weather-forecast-date">23 jan</p>
                 </div>
-                <div>
-                    26&#8451;
-                    24 jan
+                <div className="weather-forecast-item">
+                    <p className="weather-forecast-temp">26&#176;</p>
+                    <Zap className="mx-auto" />
+                    <p className="weather-forecast-date">24 jan</p>
                 </div>
             </div>
         </div>
